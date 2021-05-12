88cf3a3ff787c72d6b61df379d4c8d098a34851c
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-05-12 17:33:37 +0200

    agregamos la api del tiempo y la logica y html.

--- chistes.js
+++ chistes.js
@@ -12,3 +12,18 @@ function mostrarChiste() {
       document.getElementById("chistes").innerHTML = "“" + json.joke + "”";
     });
 }
+
+// Aqui empieza la API para el tiempo Meteorilogico
+// APi https://www.el-tiempo.net/api/json/v2/provincias/08/municipios/08019
+
+fetch("https://www.el-tiempo.net/api/json/v2/provincias/08/municipios/08019")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log("tiempo", json.temperatura_actual);
+    document.getElementById("tiempo").innerHTML =
+      "Tiempo en Barcelona: " +
+      json.stateSky.description +
+      " " +
+      json.temperatura_actual +
+      "º";
+  });
