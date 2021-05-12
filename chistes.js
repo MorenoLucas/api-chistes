c7606244ed4da958be051a07a1e0629f9f189888
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-05-12 16:38:50 +0200

    comentarios

--- chistes.js
+++ chistes.js
@@ -1,4 +1,6 @@
 let chiste;
+// funcion que al hacer click, se conecta a la API y muestra el chiste en pantalla
+// no se me asigna a la variable
 function mostrarChiste() {
   fetch("https://icanhazdadjoke.com/", {
     headers: { Accept: "application/json" },
