b84ea39da49400290a19c6c7d15973f15dfde474
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-05-13 17:35:55 +0200

    async

--- chistes.js
+++ chistes.js
@@ -1,8 +1,8 @@
 let chiste;
 // funcion que al hacer click, se conecta a la API y muestra el chiste en pantalla
 // no se me asigna a la variable
-function mostrarChiste() {
-  fetch("https://icanhazdadjoke.com/", {
+async function mostrarChiste() {
+  await fetch("https://icanhazdadjoke.com/", {
     headers: { Accept: "application/json" },
   })
     .then((response) => response.json())
