9e366a40c147e767d5ea6462a3cec6c286116358
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-05-14 11:37:54 +0200

    arreglos

--- chistes.js
+++ chistes.js
@@ -1,4 +1,3 @@
-let chiste;
 let gif = document.getElementById("confetti");
 gif.style.display = "none";
 // funcion que al hacer click, se conecta a la API y muestra el chiste en pantalla
@@ -9,27 +8,21 @@ async function mostrarChiste() {
   })
     .then((response) => response.json())
     .then((json) => {
-      console.log(json.joke);
-      chiste = json.joke;
       gif.style.display = "block";
       setTimeout(() => {
         gif.style.display = "none";
       }, 6000);
-      document.getElementById("chistes").innerHTML = "“" + json.joke + "”";
+      document.getElementById("chistes").innerHTML = `“ ${json.joke}”`;
     });
 }
 
 // Aqui empieza la API para el tiempo Meteorilogico
 // APi https://www.el-tiempo.net/api/json/v2/provincias/08/municipios/08019
-
 fetch("https://www.el-tiempo.net/api/json/v2/provincias/08/municipios/08019")
   .then((response) => response.json())
   .then((json) => {
     console.log("tiempo", json);
-    document.getElementById("tiempo").innerHTML =
-      "Tiempo en Barcelona: " +
-      json.stateSky.description +
-      " " +
-      json.temperatura_actual +
-      "º";
+    document.getElementById("tiempo").innerHTML = `Tiempo en Barcelona: 
+      ${json.stateSky.description} 
+      ${json.temperatura_actual}º`;
   });
